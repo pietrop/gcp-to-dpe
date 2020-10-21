8faf4882143384ec6e18658a3b7a92f134bc150f
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-10-21 15:58:49 -0400

    major refactor
    removed draftJs conversion step, also it assumes speaker diarization present

--- src/index.js
+++ src/index.js
@@ -1,25 +1,87 @@
-const  gcpTranscribeToDraft = require('./stt-adapter-gcp-to-draftjs/index.js');
-const draftToDigitalPaperEdit  = require('./draftjs-to-digital-paper-edit/index.js');
-
 /**
- * Converts = require(' gcp transcribe to DPE. 
- * internally it goes via draftJS to do the conversion. 
- * This could be refactored to go directly form gcp to DPE
+ * GCP does not provide a nanosecond attribute if the word starts at 0 nanosecond
+ * @param startSecond
+ * @param nanoSecond
+ * @returns {number}
+ */
+const computeTimeInSeconds = (startSecond, nanoSecond) => {
+  const NANO_SECOND = 1000000000;
+  let seconds = parseFloat(startSecond);
+
+  if (nanoSecond !== undefined) {
+    seconds = seconds + parseFloat(nanoSecond / NANO_SECOND);
+  }
+
+  return seconds;
+};
+/**
+ * Converts =  gcp STT with speaker info to DPE format.
  * @param {json} gcpTranscript - gcp Transcription Json
  * @returns {json} json transcription in DPE format
  */
 
-const gcpToDpe = (gcpTranscript) => {
-    const draftJsTranscript = gcpTranscribeToDraft(gcpTranscript);
-    // need to wrap result of conversion in a block for next converter
-    const blocks = {   "blocks": draftJsTranscript}
-    const dpeTranscript = draftToDigitalPaperEdit(blocks);
-    // For some reason the id is undefined :shrug:
-    dpeTranscript.words = dpeTranscript.words.map((word,index)=>{
-        word.id = index;
-        return word;
-    })
-    return dpeTranscript;
-}
+const gcpToDpe = (data) => {
+  let results;
+  try {
+    results = data.response.results;
+  } catch (e) {
+    throw new Error('Invalid input for GCP to DPE converter, expected data.response.results to be present');
+  }
+  //   console.log(data.response.results);
+  const finalResults = results[results.length - 1];
+  //   console.log('finalResults', JSON.stringify(finalResults, null, 2));
+  const gcpWords = finalResults.alternatives[0].words;
+  const words = gcpWords.map((word) => {
+    return {
+      start: computeTimeInSeconds(word.startTime),
+      end: computeTimeInSeconds(word.endTime),
+      text: word.word,
+    };
+  });
+
+  let currentSpeaker = gcpWords[0].speakerTag;
+  let currentSpeakerStartTime = gcpWords[0].startTime;
+  let currentSpeakerEndTime = gcpWords[gcpWords.length - 1].endTime;
+
+  const paragraphs = [];
+  let tmpParagraph = {
+    speaker: currentSpeaker,
+    start: currentSpeakerStartTime,
+    end: currentSpeakerEndTime,
+  };
+  gcpWords.forEach((word, index, list) => {
+    if (index === 0) {
+    }
+    if (word.speakerTag !== currentSpeaker) {
+      currentSpeaker = word.speakerTag;
+      //   tmpParagraph.speaker = word.speakerTag;
+      tmpParagraph.end = computeTimeInSeconds(word.endTime);
+      //   tmpParagraph.end = word.endTime;
+
+      paragraphs.push(tmpParagraph);
+      tmpParagraph = {};
+      if (gcpWords[index + 1]) {
+        tmpParagraph.start = computeTimeInSeconds(gcpWords[index + 1].startTime);
+        // tmpParagraph.start = gcpWords[index + 1].startTime;
+      }
+    } else {
+      tmpParagraph.start = computeTimeInSeconds(word.startTime);
+      //   tmpParagraph.start = word.startTime;
+      //   currentSpeaker = word.speakerTag;
+      tmpParagraph.speaker = word.speakerTag;
+      //   tmpParagraph.text = word.word;
+    }
+
+    // last element
+    if (index === list.length - 1) {
+      tmpParagraph.end = computeTimeInSeconds(word.endTime);
+      //   tmpParagraph.end = word.endTime;
+      paragraphs.push(tmpParagraph);
+    }
+  });
+  console.log('finalResults', JSON.stringify(words, null, 2));
+  return { words, paragraphs };
+};
 
 module.exports = gcpToDpe;
+module.exports.computeTimeInSeconds = computeTimeInSeconds;
