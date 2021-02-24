4f6988d0668bce26fb1eeeda23f8b45594e4f8fc
Pietro Passarelli <pietro.passarelli@wsj.com>
2021-02-24 00:11:50 -0500

    handle edge case
    edge case when there's one last word with a different speaker from previous one

--- src/example-usage.js
+++ src/example-usage.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const gcpToDpe = require('./index.js');
 
-const gcpTranscript = require('../sample/gcpStt.longrecognition-with-speakers.sample.json');
+// const gcpTranscript = require('../sample/gcpStt.longrecognition-with-speakers.sample.json');
+const gcpTranscript = require('../sample/gcp-possible-bug.sample.json');
 
 const res = gcpToDpe(gcpTranscript);
 
-fs.writeFileSync('./sample/output/dpe-output-diarization-long-recognition.json', JSON.stringify(res, null, 2));
+fs.writeFileSync('./sample/output/dpe-output-diarization-long-recognition-possible-bug.json', JSON.stringify(res, null, 2));
 console.log(res);
